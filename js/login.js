87dec87f7656abb60d825eab36bfc6144cc71cd4
mahmoudElwan <mahmoudelwan460@gmail.com>
2020-03-17 13:56:14 +0200

    .

--- js/login.js
+++ js/login.js
@@ -2,34 +2,7 @@ let userName=document.getElementById("userName");
 let password=document.getElementById("input");
 let btn=document.getElementById("myBtn");
 
-btn.onclick=function(){
-    if(reg()==true)
-        {
-             alert("Done");
-        }
-    else
-        {
-            
-            alert(" Invalid username or password ");
-            return false;
-            
-        }
-}
 
-function reg(){
-    let regx1=/^[A-z]{3,15}/;
-    let regx3=/^[0-9]{9,10}[A-Z]{1,2}/;
-    if(regx1.test(userName.value)==false||regx3.test(password.value)==false)
-        {
-    
-          return false;   
-        }
-    else
-        {
-            return true;
-        }
-    
-}
 
 $(".icone").click(function(){
     
@@ -46,36 +19,3 @@ $(".icone").click(function(){
              x.attr("type","password")
         }
 })
-/*function showPassword() {
-  let x = document.getElementById("input");
-  if (x.type === "password") {
-    x.type = "text";
-  }
-    else {
-    x.type = "password";
-  }
-}
-
-$(".myicone ").click(function(){
-    
-    if($("#myicone1").css("opacity")==1)
-        { 
-             $("#myicone1").css("opacity","0" ,showPassword())
-        }
-    else
-        {
-            $("#myicone1").css("opacity","1",showPassword()) 
-        }
-})
-      
-*/
-
-
-/*$(document).ready(function(){
-  $('.owl-carousel').owlCarousel(
-      
-      {
-          margin:10 ,loop:true,autoplay:true
-      }
-  );
-});*/
